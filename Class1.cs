051951bff3f527f6132159289c82efaeb045e0dc
antunesvitor <vguilhermeantunes@gmail.com>
2019-04-24 14:01:53 -0300

    first commit

---
+++ Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace SecurityAPI
+{
+    public class VerifyAccess : ActionFilterAttribute
+    {
+        private string FirstMessage;
+        private string FinalMessage;
+
+        public VerifyAccess(string firstMessage, string finalMessage){
+            FirstMessage = firstMessage;
+            FinalMessage = finalMessage;
+        }
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            Console.WriteLine($"============================={FirstMessage}===================================");
+        }
+        public override void OnResultExecuted(ResultExecutedContext context)
+        {
+            Console.WriteLine($"============================={FinalMessage}===================================");
+        }
+    }
+}
